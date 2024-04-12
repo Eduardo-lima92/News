c23e4c2288ec942555e07773c7fbf40805d7c9d4
Eduardo-lima92 <dudulima2005@hotmail.com>
2024-04-12 08:44:38 -0300

    Autenticação de senha de login e geração do token

--- back/index.js
+++ back/index.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const usuarioModel = require('./src/module/usuario/usuario.model')
 const app = express();
 app.use(express.json());
+
+app.post('/login', async (req, res) => {
+    if(!req.body.email) {
+        return res.status(400).json({ message: 'O campo email é obrigatorio' });
+    }
+    if (!req.body.senha) {
+        return res.status(400).json({ message: 'O campo senha é obrigatorio' });
+    }
+
+    const usuarioExistente = await usuarioModel.findOne({ email: req.body.email });
+
+    const senhaVerificada = bcrypt.compareSync(
+        req.body.senha,
+        usuarioExistente.senha
+        );
+
+    if (!senhaVerificada) {
+        return res.status(400).json({ message: 'E-mail ou senha incorretos'});
+    }
+
+    const token = jwt.sign({ _id: usuarioExistente._id }, 'dudu');
+    console.log(token)
+
+    return res.status(200).json({ message: 'Login realizado com sucesso'});
+
+   
+   
+
+});
 
 app.get('/usuarios', async (req, res) => {
     const usuarios = await usuarioModel.find({})
